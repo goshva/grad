1ec7cf6c8a7bd00c6d4f0f82d4457cc5e6ed85d6
goshva <sha.egor@gmail.com>
2022-02-12 18:51:48 +0300

    add cart

--- public/snipcart.js
+++ public/snipcart.js
@@ -1,7 +1,24 @@
+class Storage {
+    static saveProducts(products) {
+        localStorage.setItem('products', JSON.stringify(products));
+    }
+    static getProduct(id) {
+        let products = JSON.parse(localStorage.getItem('products'));
+        return products.find(product => product.id === id);
+    }
+    static saveCart(cart) {
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
+    static getCart() {
+        return localStorage.getItem("products")
+            ? JSON.parse(localStorage.getItem("products"))
+            : {};
+    }
+}
 function addCart(item) {
-    console.log(2)
-    //let products = JSON.parse(localStorage.getItem("products")) || [];
-    //let alreadyAdded = products.find(x => x.itemId === bt.dataset.itemId)
+  //  console.log(item,i)
+    //var products = JSON.parse(localStorage.getItem("products")) || [];
+    //var alreadyAdded = products.find(x => x.itemId === bt.dataset.itemId)
     /*if (alreadyAdded){
       console.log(bt.dataset)
       alreadyAdded.itemCount = bt.dataset.itemCount++
@@ -13,48 +30,104 @@ function addCart(item) {
     }
     */
 
-    let span = document.createElement('span');
-    span.setAttribute('class','badge badge-primary badge-pill');
-    let li = document.createElement('li');
+    var li = document.createElement('li');
     li.innerText = item['itemName'];
     li.setAttribute('class','list-group-item d-flex justify-content-between align-items-center');
-    li.appendChild(span);
+    var img = document.createElement('img');
+    img.src= item['itemImage']; 
+    img.style.width = "25%";
+    li.appendChild(img)
+    var div = document.createElement('div');
+    var span = document.createElement('span');
+    span.setAttribute('class','badge badge-primary badge-pill');
+    span.innerText = item.itemCount;
+    div.appendChild(span);
+    var trashIt = document.createElement('i');
+    trashIt.setAttribute('class','ti-trash');
+    trashIt.setAttribute("onclick", "removeProduct(\""+item['itemId']+"\")");
+    div.appendChild(trashIt);
+    li.appendChild(div);
     cartList.appendChild(li);
-
 };
 
 counter = document.getElementsByClassName("snipcart-items-count")[0];
-let cartList = document.getElementById("cartList");
+var cartList = document.getElementById("cartList");
 el = document.getElementsByClassName("snipcart-add-item")[0];
 el.addEventListener("click", function(){addProduct(this.dataset)}, false);
+eq = document.getElementsByClassName("equaring")[0];
+eq.addEventListener("click", function(){redirectPay()}, false);
 
 function addProduct(item){
-    console.log(1)
-    let products = [];
+    var products = {};
     if(localStorage.getItem('products')){
-        products = JSON.parse(localStorage.getItem('products'));
-        while (cartList.firstChild) {
-            cartList.firstChild.remove()
-        }
-        for (var i = 0; i < products.length; i++){
-            addCart(products[i])
-        }
-        
+        products = Storage.getCart();
     }
-    products.push(item);
-    localStorage.setItem('products', JSON.stringify(products));
+    if (item.itemId in products){
+        item.itemCount++
+    }
+    products[item.itemId]=item;
+    Storage.saveProducts(products);
     console.log(products)
-    addCart(item)
-    counter.innerText = products.length;
+    renewCart(products)
+    counter.innerText = Object.keys('products').length;
+}
+function renewCart(){
+    let products = Storage.getCart();
+    while (cartList.firstChild) {
+        cartList.firstChild.remove()
+    }
+    for (const [key, value] of Object.entries(products)) {
+        console.log(`${key}: ${value}`);
+        addCart(value)
+    }
+    
+}
+function removeProduct(id){
+
+    
+    var products = Storage.getCart();
+    console.log(products[id]["itemCount"])
+
+    if (products[id]["itemCount"] <= 1){
+        delete products[id];
+    }
+    else{
+        products[id]["itemCount"]--
+        el.dataset.itemCount--
+    }
+    
+    Storage.saveProducts(products);
+    /*
+    if (number === 0 ) {
+        
+        localStorage.setItem('products', []);
+    } else {
+        var storageProducts = JSON.parse(localStorage.getItem('products'));
+        console.log(storageProducts[number])
+        var products = storageProducts.splice(number,1);//filter(product => product.productId !== productId );
+        console.log(products)
+        console.log(storageProducts)
+        localStorage.setItem('products', JSON.stringify(products));
+    }
+*/
+
+    renewCart()
+}
+function redirectPay(){
+    let amount = "6000"
+    let orderId = "232"
+    let url = "https://grad-masterov.ru/sber.php?orderId="+orderId+"&amount="+amount
+
+    window.location.href = url;
 
 }
-function removeProduct(productId){
-
-    // Your logic for your app.
-
-    // strore products in local storage
-
-    let storageProducts = JSON.parse(localStorage.getItem('products'));
-    let products = storageProducts.filter(product => product.productId !== productId );
-    localStorage.setItem('products', JSON.stringify(products));
-}
+String.prototype.hashCode = function() {
+    var hash = 0, i, chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+      chr   = this.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  };
